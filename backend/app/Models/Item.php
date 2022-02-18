2a6d482c825c658ea7d81c0ce15e22d29c6568da
Reddemon1 <rionaldo0567@gmail.com>
2022-02-18 15:20:13 +0700

    Making API (Item , Category , Type , Location)

---
+++ backend/app/Models/Item.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\LocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource("types", TypeController::class);
+Route::resource("categories", CategoryController::class);
+Route::resource("locations", LocationController::class);
+
+Route::resource("items", ItemController::class);
